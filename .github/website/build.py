1a2a917dd04c950d27ec64870f148e453d39a536
Bryce Mecum <petridish@gmail.com>
2021-08-11 17:13:21 -0800

    New version of CI workflow

--- .github/website/build.py
+++ .github/website/build.py
@@ -6,26 +6,59 @@ import pylode
 from jinja2 import Template
 
 
-BASEDIR = Path(".", "website")
-GLOB_PATTERN = "./*/*.owl"
-INDEX_TEMPLATE = Path(".", "website", "index.template")
-INDEX_HTML = Path(".", "website", "index.html")
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+ONTOLOGIES_FILE = os.path.join(BASEDIR, "../ontologies.txt")
+INDEX_TEMPLATE = os.path.join(BASEDIR, "index.template")
 
-def get_tmp_dir():
-    return Path(BASEDIR, "tmp")
+def get_build_dir():
+    return Path(BASEDIR, "_build")
 
-def create_tmp_dir():
-    tmpdir = get_tmp_dir()
+
+def create_build_dir():
+    tmpdir = get_build_dir()
 
     if not os.path.exists(tmpdir):
         os.mkdir(tmpdir)
 
+
+def get_ontologies():
+    with open(ONTOLOGIES_FILE) as onto_file:
+        files = [tuple(line.rstrip("\n").split("\t")) for line in onto_file.readlines()]
+        return [{"name": file[0], "title": file[1], "path": file[2]} for file in files]
+
+
+def parse_ontology(path):
+    return {
+        "name": path.replace(".html", ""),
+        "path": path
+    }
+
+
+def process_ontology(ontology):
+    path = ontology["path"]
+    print(f"Processing {path}...")
+
+    # Copy owl file to build dir so it's available
+    newpath = Path(get_build_dir(), os.path.basename(path))
+    shutil.copy(path, newpath)
+
+    # Try processing once. If it fails, try processing as Turtle
+    try:
+        return run_pylode(newpath)
+    except Exception:
+        print("Re-processing as Turtle...")
+        renamedpath = os.path.join(get_build_dir(), os.path.basename(newpath).replace(".owl", ".ttl"))
+        os.rename(newpath, renamedpath)
+
+        return run_pylode(renamedpath)
+
+
 def run_pylode(path):
-    out_path = f"{path}.html"
+    out_path = os.path.join(get_build_dir(), f"{path}.html")
     out_path = out_path.replace(".ttl", "")
     out_path = out_path.replace(".owl", "")
 
-    print(f"Running PyLODE on {path}")
+    print(f"Running PyLODE on {path} with output path of {out_path}")
     html = pylode.MakeDocco(
         input_data_file=str(path), outputformat="html", profile="dataone"
     ).document()
@@ -35,26 +68,15 @@ def run_pylode(path):
 
     return out_path
 
-def process(path):
-    print(f"Processing {path}...")
 
-    newpath = Path(get_tmp_dir(), os.path.basename(path))
-    shutil.copy(path, newpath)
+def process_ontologies():
+    for ontology in get_ontologies():
+        process_ontology(ontology)
 
-    # Try processing one. If it fails, try processing as Turtle
-    try:
-        return run_pylode(newpath)
-    except Exception:
-        print("Re-processing as Turtle...")
-        renamedpath = Path(get_tmp_dir(), os.path.basename(newpath).replace(".owl", ".ttl"))
-        os.rename(newpath, renamedpath)
-
-        return run_pylode(renamedpath)
-
-def copy_all_html():
+def copy_pylode_output():
     new_paths = []
 
-    for path in Path(get_tmp_dir()).glob("*.html"):
+    for path in get_build_dir().glob("*.html"):
         print(f"Copying HTML for {path}")
 
         basename = os.path.basename(path)
@@ -64,14 +86,11 @@ def copy_all_html():
 
     return new_paths
 
-def parse_ontology(path):
-    return {
-        "name": path.replace(".html", ""),
-        "path": path
-    }
 
-def build_index(html_files):
-    ontologies = [parse_ontology(file) for file in html_files]
+def build_index():
+    print("Building index...")
+    ontologies = get_ontologies()
+
     output = None
 
     with open(INDEX_TEMPLATE, "r") as index_template:
@@ -79,18 +98,14 @@ def build_index(html_files):
         tmpl = Template(tmpl_str)
         output = tmpl.render(ontologies=ontologies)
 
-    with open(INDEX_HTML, "w") as index_html:
+    with open(os.path.join(get_build_dir(), "index.html"), "w") as index_html:
         index_html.write(str(output))
 
 
 def main():
-    create_tmp_dir()
-
-    for path in Path(".").glob(GLOB_PATTERN):
-        process(path)
-
-    html_files = copy_all_html()
-    build_index(html_files)
+    create_build_dir()
+    process_ontologies()
+    build_index()
 
 if __name__ == "__main__":
     main()
